c21a22f36851789f2eb27d837c1dd8e741db2546
tbrekalo <t@tbrekalo.xyz>
2022-03-19 17:30:52 +0000

    sequence loading done

---
+++ include/camel/io.h
@@ -0,0 +1,27 @@
+#ifndef CAMEL_IO_H_
+#define CAMEL_IO_H_
+
+#include <filesystem>
+#include <memory>
+#include <vector>
+
+#include "biosoup/nucleic_acid.hpp"
+#include "thread_pool/thread_pool.hpp"
+
+/* export header */
+#include "camel/export.h"
+/* export header */
+
+namespace camel {
+
+CAMEL_EXPORT auto LoadSequences(std::filesystem::path const& path)
+    -> std::vector<std::unique_ptr<biosoup::NucleicAcid>>;
+
+CAMEL_EXPORT auto LoadSequences(
+    std::shared_ptr<thread_pool::ThreadPool> thread_pool,
+    std::vector<std::filesystem::path> const& paths)
+    -> std::vector<std::unique_ptr<biosoup::NucleicAcid>>;
+
+}  // namespace camel
+
+#endif /* CAMEL_IO_H_ */
