a6d62cb907c0eb6577419a0f58936b0e44e45033
tbrekalo <brekalo.tvrtko@gmail.com>
2022-04-05 14:15:57 +0000

    minor changes to binding scripts; diagnosed that the issue is with the python version

--- src/bind.cc
+++ src/bind.cc
@@ -12,35 +12,29 @@ namespace nb = nanobind;
 std::atomic<std::uint32_t> biosoup::NucleicAcid::num_objects{0U};
 
 NB_MODULE(camelpy_ext, m) {
-  nb::class_<thread_pool::ThreadPool>(m, "ThreadPoolHandle")
-      .def(nb::init<std::size_t>());
-
-  m.def("create_thread_pool",
-        [](std::size_t const n_threads)
-            -> std::shared_ptr<thread_pool::ThreadPool> {
-          return std::make_shared<thread_pool::ThreadPool>(n_threads);
-        });
-
-  // TODO: sequence handles, sequence loading...
-
-  nb::class_<camel::MapCfg>(m, "MapCfg")
-      .def(nb::init<std::uint8_t, std::uint8_t, double>())
-      .def_readwrite("kmer_len", &camel::MapCfg::kmer_len)
-      .def_readwrite("win_len", &camel::MapCfg::win_len);
-
   nb::class_<camel::Coverage>(m, "Coverage")
       .def(nb::init<camel::Coverage::ValueType, camel::Coverage::ValueType,
-                    camel::Coverage::ValueType, camel::Coverage::ValueType>())
+                    camel::Coverage::ValueType,
+                    camel::Coverage::ValueType>())
       .def_readwrite("match", &camel::Coverage::mat)
       .def_readwrite("deletion", &camel::Coverage::del)
       .def_readwrite("insertion", &camel::Coverage::ins)
       .def_readwrite("mismatch", &camel::Coverage::mis);
 
   nb::class_<camel::Pile>(m, "Pile")
-      .def(nb::init<std::uint32_t, std::string, std::vector<camel::Coverage>>())
+      .def(nb::init<std::uint32_t, std::string const&,
+                    std::vector<camel::Coverage>>())
       .def_readonly("id", &camel::Pile::id)
       .def_readonly("seq_name", &camel::Pile::seq_name)
-      .def_readwrite("coverages", &camel::Pile::covgs);
+      .def_readonly("covgs", &camel::Pile::covgs);
+
+  nb::class_<thread_pool::ThreadPool>(m, "ThreadPool")
+      .def(nb::init<std::size_t>());
+
+  m.def("create_thread_pool",
+        [](std::size_t const n) -> std::shared_ptr<thread_pool::ThreadPool> {
+          return std::make_shared<thread_pool::ThreadPool>(n);
+        });
 
   m.def("serialize_piles",
         [](std::shared_ptr<thread_pool::ThreadPool> thread_pool,
