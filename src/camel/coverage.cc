d832ae76a0e5c2d34e3cbd246506978545f3a572
tbrekalo <t@tbrekalo.xyz>
2022-04-19 14:10:31 +0000

    improved overlap storage model; better sorting of overlaps into groups; started working on gether algorithm in coverage section

--- src/camel/coverage.cc
+++ src/camel/coverage.cc
@@ -17,7 +17,7 @@ namespace camel {
 
 namespace detail {
 
-static constexpr std::size_t kAlignBatchCap = 1UL << 36UL;      // ~68.7gb
+static constexpr std::size_t kAlignBatchCap = 1UL << 34UL;      // ~17.18gb
 static constexpr std::size_t kDefaultSeqGroupSz = 1UL << 32UL;  // 4.3gb
 
 }  // namespace detail
@@ -27,33 +27,39 @@ auto CalculateCoverage(
     std::vector<std::unique_ptr<biosoup::NucleicAcid>> const& reads,
     std::filesystem::path const& pile_storage_dir) -> void {
   auto ovlp_groups = FindOverlaps(thread_pool, map_cfg, reads);
+
   if (std::filesystem::exists(pile_storage_dir)) {
     std::filesystem::remove_all(pile_storage_dir);
   }
+
+  auto find_batch_last = [&reads](
+                             std::vector<ReadIdOvlpCnt>::const_iterator first,
+                             std::vector<ReadIdOvlpCnt>::const_iterator last,
+                             std::size_t const batch_cap)
+      -> std::vector<ReadIdOvlpCnt>::const_iterator {
+    for (auto batch_sz = 0UL; first != last && batch_sz < batch_cap; ++first) {
+      batch_sz += reads[first->read_id]->inflated_len * sizeof(Coverage) +
+                  first->n_overlaps * sizeof(biosoup::Overlap);
+    }
+
+    return first;
+  };
 
   std::filesystem::create_directory(pile_storage_dir);
   auto timer = biosoup::Timer();
 
   // sort groups ascending by read size
   {
-    auto group_with_sizes =
-        std::vector<std::pair<std::size_t, std::vector<biosoup::Overlap>>>();
+    auto group_with_sizes = std::vector<std::pair<std::size_t, Group>>();
 
     auto const calc_group_sz =
         [&reads](
-            std::vector<biosoup::Overlap>::const_iterator first,
-            std::vector<biosoup::Overlap>::const_iterator last) -> std::size_t {
-      auto unique_read_ids = tsl::robin_set<std::size_t>();
-      for (auto it = first; it != last; ++it) {
-        unique_read_ids.insert(it->lhs_id);
-        unique_read_ids.insert(it->rhs_id);
-      }
-
+            std::vector<ReadIdOvlpCnt>::const_iterator first,
+            std::vector<ReadIdOvlpCnt>::const_iterator last) -> std::size_t {
       return std::transform_reduce(
-          unique_read_ids.cbegin(), unique_read_ids.cend(), 0UL,
-          std::plus<std::size_t>(),
-          [&reads](std::uint32_t const read_id) -> std::size_t {
-            return reads[read_id]->inflated_len;
+          first, last, 0UL, std::plus<std::size_t>(),
+          [&reads](ReadIdOvlpCnt r_id_ovlp_cnt) -> std::size_t {
+            return reads[r_id_ovlp_cnt.read_id]->inflated_len;
           });
     };
 
@@ -64,51 +70,31 @@ auto CalculateCoverage(
         std::make_move_iterator(ovlp_groups.begin()),
         std::make_move_iterator(ovlp_groups.end()),
         std::back_inserter(group_with_sizes),
-        [&calc_group_sz](std::vector<biosoup::Overlap> ovlps)
-            -> std::pair<std::size_t, std::vector<biosoup::Overlap>> {
-          return std::pair(calc_group_sz(ovlps.cbegin(), ovlps.cend()),
-                           std::move(ovlps));
+        [&calc_group_sz](Group&& group) -> std::pair<std::size_t, Group> {
+          return std::pair(calc_group_sz(group.read_n_ovlps.cbegin(),
+                                         group.read_n_ovlps.cend()),
+                           std::move(group));
         });
 
     ovlp_groups.clear();
-    std::sort(
-        group_with_sizes.begin(), group_with_sizes.end(),
-        [](std::pair<std::size_t, std::vector<biosoup::Overlap>> const& lhs,
-           std::pair<std::size_t, std::vector<biosoup::Overlap>> const& rhs)
-            -> bool { return lhs.first < rhs.first; });
+    std::sort(group_with_sizes.begin(), group_with_sizes.end(),
+              [](std::pair<std::size_t, Group> const& lhs,
+                 std::pair<std::size_t, Group> const& rhs) -> bool {
+                return lhs.first < rhs.first;
+              });
 
-    std::transform(
-        std::make_move_iterator(group_with_sizes.begin()),
-        std::make_move_iterator(group_with_sizes.end()),
-        std::back_inserter(ovlp_groups),
-        [](std::pair<std::size_t, std::vector<biosoup::Overlap>> pso)
-            -> std::vector<biosoup::Overlap> { return std::move(pso.second); });
+    std::transform(std::make_move_iterator(group_with_sizes.begin()),
+                   std::make_move_iterator(group_with_sizes.end()),
+                   std::back_inserter(ovlp_groups),
+                   [](std::pair<std::size_t, Group>&& psg) -> Group {
+                     return std::move(psg.second);
+                   });
 
     fmt::print(
         stderr,
         "[camel::CalculateCoverage]({:12.3f}) sorted overlap groups by size\n",
         timer.Stop());
   }
-
-  // auto group_sort_futures = std::vector<std::future<void>>();
-
-  // auto const ovlp_cmp_by_lhs_id = [](biosoup::Overlap const& a,
-  //                                    biosoup::Overlap const& b) {
-  //   return a.lhs_id < b.rhs_id;
-  // };
-
-  // auto const sort_ovlp_range =
-  //     [](std::vector<biosoup::Overlap>::const_iterator first,
-  //        std::vector<biosoup::Overlap>::const_iterator last,
-  //        auto&& ovlp_cmp_fn) -> void {
-  //   static_assert(
-  //       detail::IsOverlapCmpCallableV<decltype(ovlp_cmp_fn)>,
-  //       "[camel::FindOverlaps::sort_ovlp_range] ovlp_cmp_fn must be callable"
-  //       "with signature (biosoup::Overlap const&, biosoup::Overlap const&) ->
-  //       " "bool");
-
-  //   std::sort(first, last, std::forward<decltype(ovlp_cmp_fn)>(ovlp_cmp_fn));
-  // };
 
   auto active_piles = tsl::robin_map<std::uint32_t, Pile>();
 
@@ -123,29 +109,45 @@ auto CalculateCoverage(
     }
   };
 
-  auto const refresh_active_piles =
-      [&reads, &active_piles, &try_init_pile](
-          std::vector<biosoup::Overlap>::const_iterator first,
-          std::vector<biosoup::Overlap>::const_iterator last) -> void {
-    active_piles.clear();
-    for (; first != last; ++first) {
-      try_init_pile(reads[first->lhs_id]);
-      try_init_pile(reads[first->rhs_id]);
+  auto parallel_gather =
+      [&thread_pool](
+          ReadIdOvlpCnt const& read_info,
+          Group const& active_group) -> std::vector<biosoup::Overlap> {
+    auto dst = std::vector<biosoup::Overlap>();
+    dst.resize(read_info.n_overlaps);
+
+    auto gather_futures =
+        std::vector<std::future<std::vector<biosoup::Overlap>>>();
+
+    auto empty_idx = std::atomic<std::size_t>();
+    for (auto const& ovlp_vec : active_group.ovlp_vecs) {
+      gather_futures.emplace_back(thread_pool->Submit(
+          [&dst, &empty_idx](
+              std::uint32_t const read_id,
+              std::vector<biosoup::Overlap> const& ovlp_vec) -> void {
+            for (auto const& ovlp : ovlp_vec) {
+              if (ovlp.lhs_id == read_id) {
+                dst[++empty_idx] = ovlp;
+              } else if (ovlp.rhs_id == read_id) {
+                dst[++empty_idx] = detail::ReverseOverlap(ovlp);
+              }
+            }
+          },
+          read_info.read_id, ovlp_vec));
     }
+
+    return dst;
   };
 
-  // auto const overlap_strings = 
-  //   [&reads](biosoup::Overlap const& ovlp) -> 
-  //     std::pair<std::string, std::string> {
 
-  // 
-  // };
-
-  for (auto& ovlp_group : ovlp_groups) {
-    refresh_active_piles(ovlp_group.cbegin(), ovlp_group.cend());
-
-    ovlp_group.clear();
-    ovlp_group.shrink_to_fit();
+  for (auto g_id = 0U; g_id < ovlp_groups.size(); ++g_id) {
+    auto active_group = std::move(ovlp_groups[g_id]);
+    for (auto batch_first = active_group.read_n_ovlps.cbegin();
+         batch_first != active_group.read_n_ovlps.end();) {
+      auto batch_end =
+          find_batch_last(batch_first, active_group.read_n_ovlps.cend(),
+                          detail::kAlignBatchCap);
+    }
   }
 }
 
