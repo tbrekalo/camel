4b2ce054209594ea63c08684918fcf9b6f493d09
tbrekalo <brekalo.tvrtko@gmail.com>
2022-08-20 17:08:31 +0000

    added overlap loading

--- src/camel/io.cc
+++ src/camel/io.cc
@@ -11,7 +11,10 @@
 
 #include "bioparser/fasta_parser.hpp"
 #include "bioparser/fastq_parser.hpp"
+#include "bioparser/paf_parser.hpp"
+#include "detail/overlap.h"
 #include "fmt/core.h"
+#include "tsl/robin_map.h"
 
 namespace camel {
 
@@ -57,6 +60,71 @@ static auto CmpNucleicAcidByName(
     std::unique_ptr<biosoup::NucleicAcid> const& rhs) -> bool {
   return lhs->name < rhs->name;
 }
+
+/* clang-format off */
+struct PafOverlap {
+
+  PafOverlap() = default;
+
+  PafOverlap(PafOverlap const&) = default;
+  PafOverlap& operator=(PafOverlap const&) = default;
+
+  PafOverlap(PafOverlap&&) = default;
+  PafOverlap& operator=(PafOverlap&&) = default;
+
+  PafOverlap(
+    char const* query_name,
+    std::uint32_t query_name_len,
+
+    std::uint32_t query_len,
+    std::uint32_t query_start,
+    std::uint32_t query_end,
+
+    char relative_strand,
+
+    char const* target_name,
+    std::uint32_t target_name_len,
+
+    std::uint32_t target_len,
+    std::uint32_t target_start,
+    std::uint32_t target_end,
+    
+    std::uint32_t num_matches,
+    std::uint32_t alignment_len,
+    std::uint32_t mapping_quality) :
+      query_name(query_name, query_name_len),
+      query_len(query_len),
+      query_start(query_start),
+      query_end(query_end),
+
+      relative_strand(relative_strand),
+
+      target_name(target_name, target_name_len),
+      target_len(target_len),
+      target_start(target_start),
+      target_end(target_end),
+
+      num_matches(num_matches),
+      alignment_len(alignment_len),
+      mapping_quality(mapping_quality) {}
+  
+  std::string query_name;
+  std::uint32_t query_len;
+  std::uint32_t query_start;
+  std::uint32_t query_end;
+
+  char relative_strand;
+
+  std::string target_name;
+  std::uint32_t target_len;
+  std::uint32_t target_start;
+  std::uint32_t target_end;
+
+  std::uint32_t num_matches;
+  std::uint32_t alignment_len;
+  std::uint32_t mapping_quality;
+};
+/* clang-format on */
 
 }  // namespace detail
 
@@ -210,6 +278,58 @@ auto LoadSequences(State& state,
   decltype(dst)(std::make_move_iterator(dst.begin()),
                 std::make_move_iterator(dst.end()))
       .swap(dst);
+
+  return dst;
+}
+
+auto LoadOverlaps(
+    std::filesystem::path const& paf_path,
+    std::vector<std::unique_ptr<biosoup::NucleicAcid>> const& reads)
+    -> std::vector<std::vector<biosoup::Overlap>> {
+  auto dst = std::vector<std::vector<biosoup::Overlap>>(reads.size());
+  auto name_to_id =
+      tsl::robin_map<std::string_view, std::uint32_t>(reads.size());
+
+  for (auto const& read : reads) {
+    name_to_id[read->name] = read->id;
+  }
+
+  auto const transform_overlap =
+      [&name_to_id](
+          std::unique_ptr<detail::PafOverlap> paf_ovlp) -> biosoup::Overlap {
+    return biosoup::Overlap(name_to_id.at(paf_ovlp->query_name),
+                            paf_ovlp->query_start, paf_ovlp->query_end,
+
+                            name_to_id.at(paf_ovlp->target_name),
+                            paf_ovlp->target_start, paf_ovlp->target_end,
+                            paf_ovlp->num_matches,
+                            paf_ovlp->relative_strand == '+');
+  };
+
+  {
+    auto best_ovlps = std::vector<biosoup::Overlap>(reads.size());
+    {
+      auto parser =
+          bioparser::Parser<detail::PafOverlap>::Create<bioparser::PafParser>(
+              paf_path.string());
+
+      auto ovlps =
+          parser->Parse(std::numeric_limits<std::uint64_t>::max(), true);
+
+      for (auto& ovlp_ptr : ovlps) {
+        auto ovlp = transform_overlap(std::move(ovlp_ptr));
+        if (detail::OverlapError(ovlp) < 0.2 &&
+            detail::OverlapLength(best_ovlps[ovlp.rhs_id]) <
+                detail::OverlapLength(ovlp)) {
+          best_ovlps[ovlp.rhs_id] = ovlp;
+        }
+      }
+    }
+
+    for (auto const& it : best_ovlps) {
+      dst[it.lhs_id].push_back(it);
+    }
+  }
 
   return dst;
 }
