b95aa2ae0d98c0d4d5722fdffab8b8feaab298c2
tbrekalo <brekalo.tvrtko@gmail.com>
2022-09-03 19:52:26 +0000

    adjusted alignment task packing; added nodiscard to task queue push

--- src/camel/detail/alignment.h
+++ src/camel/detail/alignment.h
@@ -3,19 +3,24 @@
 
 #include <memory>
 
+#include "biosoup/nucleic_acid.hpp"
 #include "biosoup/overlap.hpp"
 #include "edlib.h"
-#include "nucleic_view.h"
+#include "nonstd/span.hpp"
 
 namespace camel::detail {
 
 [[nodiscard]] auto ExtractSubstrings(
-    std::vector<std::unique_ptr<biosoup::NucleicAcid>> const& reads,
-    biosoup::Overlap const& ovlp) -> std::pair<std::string, std::string>;
+    nonstd::span<std::unique_ptr<biosoup::NucleicAcid>> reads,
+    biosoup::Overlap ovlp) -> std::tuple<std::string, std::string>;
 
 [[nodiscard]] auto AlignStrings(std::string_view lhs_str_view,
-                                       std::string_view rhs_str_view)
+                                std::string_view rhs_str_view)
     -> EdlibAlignResult;
+
+[[nodiscard]] auto OverlapToALignment(
+    nonstd::span<std::unique_ptr<biosoup::NucleicAcid>> reads,
+    biosoup::Overlap ovlp) -> EdlibAlignResult;
 
 }  // namespace camel::detail
 
