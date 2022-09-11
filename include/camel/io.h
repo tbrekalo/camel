1f909bdd9dd5550ec5c4e0809a4815420f7f31ae
tbrekalo <t@tbrekalo.xyz>
2022-09-11 07:40:27 +0000

    added overlap cap option

--- include/camel/io.h
+++ include/camel/io.h
@@ -52,7 +52,8 @@ CAMEL_EXPORT auto StoreSequences(
  */
 CAMEL_EXPORT [[nodiscard]] auto LoadOverlaps(
     std::filesystem::path const& paf_path,
-    std::vector<std::unique_ptr<biosoup::NucleicAcid>> const& reads)
+    std::vector<std::unique_ptr<biosoup::NucleicAcid>> const& reads,
+    std::size_t const n_overlaps)
     -> std::vector<std::vector<biosoup::Overlap>>;
 
 }  // namespace camel
