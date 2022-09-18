d99aeaeaf0cce7d0e90705b01f20132336227f9c
tbrekalo <brekalo.tvrtko@gmail.com>
2022-09-18 12:23:18 +0000

    simplified call sites

--- src/camel/detail/call_sites.h
+++ src/camel/detail/call_sites.h
@@ -5,42 +5,14 @@
 
 namespace camel::detail {
 
-[[nodiscard]] auto IsUnstableSite(CoverageSignals const& covg,
-                                  std::uint32_t const covg_estimate,
-                                  double const strong_base_ratio,
-                                  double const indle_ratio) -> bool;
+auto IsStableSite(CoverageSignals const& covg_signals,
+                  std::uint32_t const covg_estimate,
+                  std::uint8_t const base_code, double const min_match_rate,
+                  double const max_insertion_rate) -> bool;
 
-[[nodiscard]] auto IsSnpSite(CoverageSignals const& covg,
-                             std::uint32_t const covg_estimate) -> bool;
-
-[[nodiscard]] auto IsErrorSite(CoverageSignals const& covg,
-                               std::uint32_t const covg_estimate) -> bool;
-
-template <class PredFn>
-[[nodiscard]] auto CallSites(std::vector<CoverageSignals> const& covg,
-                             std::uint32_t const covg_estimate, PredFn pred_fn)
-    -> std::enable_if_t<
-        std::is_invocable_r_v<bool, PredFn, CoverageSignals const,
-                              std::uint32_t const>,
-        std::vector<std::uint32_t>> {
-  auto buff = std::vector<std::uint32_t>();
-  buff.reserve(buff.size() / 100);
-  for (auto i = 0U; i < covg.size(); ++i) {
-    if (pred_fn(covg[i], covg_estimate)) {
-      buff.push_back(i);
-    }
-  }
-
-  return decltype(buff)(buff.begin(), buff.end());
-}
-
-[[nodiscard]] auto CallSnpCandidates(std::vector<CoverageSignals> const& covg,
-                                     std::uint32_t const covg_estimate)
-    -> std::vector<std::uint32_t>;
-
-[[nodiscard]] auto CallErrorSites(std::vector<CoverageSignals> const& covg,
-                                  std::uint32_t const covg_estimate)
-    -> std::vector<std::uint32_t>;
+auto IsSnpSite(CoverageSignals const& covg_signals,
+               std::uint32_t const covg_estimate, double const dominance_ratio)
+    -> bool;
 
 }  // namespace camel::detail
 
