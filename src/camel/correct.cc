e18bfb1f9d29e3b0ce4a930a0507accb1603e297
tbrekalo <t@tbrekalo.xyz>
2022-09-18 20:04:56 +0000

    deprecated task queue

--- src/camel/correct.cc
+++ src/camel/correct.cc
@@ -8,27 +8,11 @@
 
 #include "biosoup/timer.hpp"
 #include "detail/coverage.h"
-#include "detail/overload.h"
-#include "detail/task_queue.h"
+#include "detail/window.h"
 #include "fmt/core.h"
-#include "tsl/robin_map.h"
+#include "tbb/parallel_for.h"
 
 namespace camel {
-
-namespace detail {
-
-static auto constexpr kUpdateInterval = 1. / 24.;
-
-struct ConsensuState {
-  std::string backbone;
-  std::vector<ReferenceWindow> ref_windows;
-  std::vector<std::string> window_consensus;
-
-  std::size_t n_awaiting_tasks;
-  tsl::robin_map<TaskIdType, std::uint32_t> task_mapping;
-};
-
-}  // namespace detail
 
 auto ErrorCorrect(CorrectConfig const correct_cfg,
                   std::vector<std::unique_ptr<biosoup::NucleicAcid>> reads,
@@ -44,204 +28,100 @@ auto ErrorCorrect(CorrectConfig const correct_cfg,
              function_timer.Stop(), coverage_estimate);
 
   function_timer.Start();
-  auto const n_targets = std::transform_reduce(
-      overlaps.cbegin(), overlaps.cend(), 0UL, std::plus<size_t>(),
-      [](std::vector<biosoup::Overlap> const& ovlp_vec) -> size_t {
-        return !ovlp_vec.empty();
-      });
+  auto target_ids = std::vector<std::size_t>();
+  for (auto i = 0U; i < overlaps.size(); ++i) {
+    if (!overlaps[i].empty()) {
+      target_ids.push_back(i);
+    }
+  }
 
+  auto const n_targets = target_ids.size();
   fmt::print(stderr, "[camel::ErrorCorrect]({:12.3f}) n target reads {}\n",
              function_timer.Stop(), n_targets);
 
-  auto n_aligned = 0U;
-  auto n_windowed = 0U;
-  auto n_polished = 0U;
-  auto n_fails = 0U;
+  auto n_aligned = std::atomic_size_t(0);
+  auto n_polished = std::atomic_size_t(0);
 
-  auto const report_status = [&]() -> void {
-    auto const to_percent = [n_targets](double n) -> double {
-      return 100. * n / n_targets;
+  auto report_ticket = std::atomic_size_t(0);
+  auto const report_state = [&function_timer, n_targets, &n_aligned,
+                             &n_polished, &report_ticket]() -> void {
+    auto const to_percent = [n_targets](double val) -> double {
+      return 100. * (val / n_targets);
     };
-    fmt::print(stderr,
-               "\r[camel::ErrorCorrect]({:12.3f}) aligned : {:3.2f}% | "
-               "converted to windows : {:3.2f}% | "
-               "polished : {:3.2f}% | failed polishes : {:3.2f}%",
-               function_timer.Lap(), to_percent(n_aligned),
-               to_percent(n_windowed), to_percent(n_polished),
-               to_percent(n_fails));
+
+    if (auto ticket = ++report_ticket; ticket == report_ticket) {
+      fmt::print(stderr,
+                 "\r[camel::ErrorCorrect]({:12.3f}) aligned {:3.3f} | polished "
+                 "{:3.3f}",
+                 function_timer.Lap(), to_percent(n_aligned),
+                 to_percent(n_polished));
+    }
   };
 
-  auto task_queue = detail::TaskQueue();
-
-  auto alignment_registry =
-      tsl::robin_map<detail::TaskIdType,
-                     std::pair<std::uint32_t, std::uint32_t>>(
-          reads.size() * coverage_estimate);
-  auto alignment_counts = std::vector<std::uint32_t>(reads.size());
-
-  auto windowing_registry = tsl::robin_map<detail::TaskIdType, std::uint32_t>(
-      reads.size() * coverage_estimate);
-  auto consensus_registry = tsl::robin_map<detail::TaskIdType, std::uint32_t>(
-      reads.size() * coverage_estimate);
-
-  auto alignments = std::vector<std::vector<EdlibAlignResult>>(reads.size());
-  auto consensus_states = std::vector<detail::ConsensuState>(reads.size());
-
-  for (auto i = 0U; i < reads.size(); ++i) {
-    alignments[i].resize(overlaps[i].size());
-    alignment_counts[i] = overlaps[i].size();
-  }
-
-  for (auto read_idx = 0U; read_idx < reads.size(); ++read_idx) {
-    if (!overlaps[read_idx].empty()) {
-      for (auto ovlp_idx = 0U; ovlp_idx < overlaps[read_idx].size();
-           ++ovlp_idx) {
-        alignment_registry[task_queue.Push(
-            detail::AlignmentArgPack(reads, overlaps[read_idx][ovlp_idx]))] =
-            std::pair(read_idx, ovlp_idx);
-      }
-    } else {
-      dst.push_back(std::make_unique<biosoup::NucleicAcid>(*reads[read_idx]));
-    }
-  }
+  dst.resize(n_targets);
 
   function_timer.Start();
-  {
-    auto update_timer = biosoup::Timer();
+  tbb::parallel_for(
+      std::size_t(0), target_ids.size(), [&](std::size_t target_index) -> void {
+        auto const read_id = target_ids[target_index];
+        auto alignments =
+            std::vector<EdlibAlignResult>(overlaps[read_id].size());
+        tbb::parallel_for(std::size_t(0), overlaps[read_id].size(),
+                          [&, read_id](std::size_t overlap_id) -> void {
+                            alignments[overlap_id] = detail::OverlapToALignment(
+                                reads, overlaps[read_id][overlap_id]);
+                          });
+        ++n_aligned;
+        report_state();
 
-    update_timer.Start();
-    while (n_polished + n_fails < n_targets) {
-      if (auto opt_result = task_queue.TryPop(); opt_result) {
-        std::visit(
-            detail::overload{
-                [&, task_id = opt_result->task_id](
-                    detail::AlignmentResult alignment) -> void {
-                  auto const [read_id, ovlp_id] = alignment_registry[task_id];
+        auto windows = detail::CreateWindowsFromAlignments(
+            reads, overlaps[read_id], alignments, coverage_estimate);
 
-                  alignments[read_id][ovlp_id] = alignment;
-                  if (--alignment_counts[read_id] == 0U) {
-                    windowing_registry[task_queue.Push(
-                        detail::WindowConstructArgPack(
-                            reads, overlaps[read_id], alignments[read_id],
-                            correct_cfg.correct_window, coverage_estimate))] =
-                        read_id;
-                    if (++n_aligned == n_targets) {
-                      std::remove_cvref_t<decltype(alignment_registry)>().swap(
-                          alignment_registry);
-                      std::remove_cvref_t<decltype(alignment_counts)>().swap(
-                          alignment_counts);
-                    }
-                  }
-                },
-                [&, task_id = opt_result->task_id](
-                    detail::WindowConstructResult ref_windows) -> void {
-                  auto const read_id = windowing_registry[task_id];
-                  auto& consensus_state = consensus_states[read_id];
-                  auto const n_windows = ref_windows.size();
+        auto backbone = reads[read_id]->InflateData();
+        auto window_consensus = std::vector<std::string>(windows.size());
 
-                  if (n_windows != 0) {
-                    {
-                      consensus_state = detail::ConsensuState{
-                          .backbone = reads[read_id]->InflateData(),
-                          .ref_windows = std::move(ref_windows),
-                          .window_consensus =
-                              std::vector<std::string>(n_windows),
-                          .n_awaiting_tasks = n_windows,
-                          .task_mapping =
-                              tsl::robin_map<detail::TaskIdType, std::uint32_t>(
-                                  n_windows)};
-                    }
+        tbb::parallel_for(
+            std::size_t(0), windows.size(), [&](std::size_t window_id) -> void {
+              auto interval = windows[window_id].interval;
+              window_consensus[window_id] = detail::WindowConsensus(
+                  std::string_view(std::next(backbone.cbegin(), interval.first),
+                                   std::next(backbone.cbegin(), interval.last)),
+                  detail::ReferenceWindowView{
+                      .interval = interval,
+                      .aligned_segments = windows[window_id].aligned_segments},
+                  correct_cfg.poa_cfg);
+            });
 
-                    for (auto win_id = 0U;
-                         win_id < consensus_state.ref_windows.size();
-                         ++win_id) {
-                      auto& ref_win = consensus_state.ref_windows[win_id];
-                      auto const win_task_id =
-                          task_queue.Push(detail::WindowConsensusArgPack(
-                              std::string_view(
-                                  std::next(consensus_state.backbone.begin(),
-                                            ref_win.interval.first),
-                                  std::next(consensus_state.backbone.begin(),
-                                            ref_win.interval.last)),
-                              detail::ReferenceWindowView{
-                                  .interval = ref_win.interval,
-                                  .aligned_segments = ref_win.aligned_segments},
-                              correct_cfg.poa_cfg));
+        {
+          auto consensus = std::string();
+          consensus.reserve(reads[read_id]->inflated_len * 1.2);
+          auto prev = 0U;
+          for (auto win_idx = 0U; win_idx < windows.size(); ++win_idx) {
+            auto const& interval = windows[win_idx].interval;
 
-                      consensus_state.task_mapping[win_task_id] = win_id;
-                      consensus_registry[win_task_id] = read_id;
-                    }
+            consensus.insert(consensus.end(), std::next(backbone.begin(), prev),
+                             std::next(backbone.begin(), interval.first));
+            consensus += window_consensus[win_idx];
+            prev = interval.last;
+          }
 
-                  } else {
-                    dst.push_back(std::make_unique<biosoup::NucleicAcid>(
-                        *reads[read_id]));
-                    ++n_fails;
-                  }
+          consensus.insert(consensus.end(), std::next(backbone.begin(), prev),
+                           backbone.end());
 
-                  ++n_windowed;
+          dst[target_index] = std::make_unique<biosoup::NucleicAcid>(
+              fmt::format("polished_{:09d}", n_polished), consensus);
+        }
 
-                  std::vector<biosoup::Overlap>{}.swap(overlaps[read_id]);
-                  std::vector<EdlibAlignResult>{}.swap(alignments[read_id]);
+        ++n_polished;
+        report_state();
 
-                  if (n_windowed + n_fails == n_targets) {
-                    std::remove_cvref_t<decltype(windowing_registry)>().swap(
-                        windowing_registry);
-                  }
-                },
-                [&, task_id = opt_result->task_id](
-                    detail::WindowConsensusResult win_consensus) -> void {
-                  auto const read_id = consensus_registry[task_id];
-                  auto& consensus_state = consensus_states[read_id];
+        overlaps[read_id].clear();
+      });
 
-                  consensus_state
-                      .window_consensus[consensus_state.task_mapping[task_id]] =
-                      std::move(win_consensus);
+  detail::ReleaseAlignmentEngines();
 
-                  if (--consensus_state.n_awaiting_tasks == 0U) {
-                    auto consensus = std::string();
-                    consensus.reserve(reads[read_id]->inflated_len * 1.2);
-                    auto prev = 0U;
-                    for (auto win_idx = 0U;
-                         win_idx < consensus_state.window_consensus.size();
-                         ++win_idx) {
-                      auto const& interval =
-                          consensus_state.ref_windows[win_idx].interval;
-
-                      consensus.insert(
-                          consensus.end(),
-                          std::next(consensus_state.backbone.begin(), prev),
-                          std::next(consensus_state.backbone.begin(),
-                                    interval.first));
-                      consensus += consensus_state.window_consensus[win_idx];
-                      prev = interval.last;
-                    }
-
-                    consensus.insert(
-                        consensus.end(),
-                        std::next(consensus_state.backbone.begin(), prev),
-                        consensus_state.backbone.end());
-
-                    dst.emplace_back(std::make_unique<biosoup::NucleicAcid>(
-                        fmt::format("polished_{:09d}", n_polished), consensus));
-
-                    consensus_state = detail::ConsensuState{};
-                    ++n_polished;
-                  }
-                }},
-            std::move(opt_result->value));
-      }
-
-      if (update_timer.Lap() > detail::kUpdateInterval) {
-        report_status();
-        update_timer.Start();
-      }
-    }
-
-    report_status();
-    fmt::print(stderr, "\n");
-    fmt::print(stderr, "[camel::ReleaseAlignmentEngines] released {} engines\n",
-               detail::ReleaseAlignmentEngines());
-  }
+  report_state();
+  fmt::print(stderr, "\n");
 
   fmt::print(stderr, "[camel::ErrorCorrect]({:12.3f})\n",
              function_timer.elapsed_time());
