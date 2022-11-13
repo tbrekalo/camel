7b17a39d444bef065ae0a09485559e249bdaebaa
tbrekalo <t@tbrekalo.xyz>
2022-11-13 19:39:49 +0000

    moved eval db storage to orm

---
+++ eval/pyeval/db/orm/models/benchmark.py
@@ -0,0 +1,16 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as sa_orm
+
+from .base import Base
+
+class Benchmark(Base,):
+    __tablename__ = 'benchmark'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    threads = sa.Column(sa.Integer, nullable=False)
+    runtime_s = sa.Column(sa.Integer, nullable=False)
+    peak_memory_mib = sa.Column(sa.Integer, nullable=False)
+    timestemp = sa.Column(sa.DateTime, default=sa.func.now())
+
+    run_id = sa.Column(sa.Integer, sa.ForeignKey('run.id'))
+    run = sa_orm.relationship('Run', back_populates='benchmark')
