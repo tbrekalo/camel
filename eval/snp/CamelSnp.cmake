169e9899fd1edaabb336b44dc9916ca8696bb1c0
tbrekalo <32259102+tbrekalo@users.noreply.github.com>
2022-11-27 13:59:24 +0100

    CAMEL-0.8.1: deprecated editdistance snp evaluation

--- eval/snp/CamelSnp.cmake
+++
@@ -1,6 +0,0 @@
-find_package(nlohmann_json)
-
-add_executable(snp_eval ${CMAKE_CURRENT_LIST_DIR}/src/main.cc)
-target_link_libraries(
-  snp_eval PRIVATE camel cxxopts::cxxopts edlib fmt::fmt
-                   nlohmann_json::nlohmann_json tsl::robin_map)
